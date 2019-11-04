9fa68036ac54c9580a3c879174b43ed73b83c058
Albert Suarez <alsumo95@gmail.com>
2019-11-04 23:55:16 +0100

    Implemented html reader and writer

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+HTML_TEMPLATE_PATH = 'src/templates/template.html'
+HTML_OUTPUT_PATH = 'index.html'
+
+
+__all__ = [
+    'HTML_TEMPLATE_PATH',
+    'HTML_OUTPUT_PATH'
+]
--- src/pokemon_web_spider.py
+++ src/pokemon_web_spider.py
@@ -1,2 +1,20 @@
-def generate():
+from src import *
+
+
+def _scrape_pokemon():
     pass
+
+
+def _render_template():
+    with open(HTML_TEMPLATE_PATH, 'r', encoding='utf-8') as template_html_file:
+        template_html_string = template_html_file.read()
+
+    # TODO
+
+    with open(HTML_OUTPUT_PATH, 'w', encoding='utf-8') as output_html_file:
+        output_html_file.write(template_html_string)
+
+
+def generate():
+    _scrape_pokemon()
+    _render_template()
